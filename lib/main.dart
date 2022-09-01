23cd98a6a3b71b6411df655874312ab0263f31f4
fahadkhan8989 <fahadkhan8989@gmail.com>
2022-09-01 23:27:27 +0500

    init

---
+++ lib/main.dart
@@ -0,0 +1,119 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+import 'story_brain.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+StoryBrain storyBrain = StoryBrain();
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Destiny"),
+        centerTitle: true,
+        backgroundColor: Colors.blue[900],
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("images/background.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        padding: EdgeInsets.symmetric(vertical: 50, horizontal: 15),
+        constraints: BoxConstraints.expand(),
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              flex: 12,
+              child: Center(
+                child: Text(
+                  storyBrain.getStory(),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 26,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.red[800],
+                  textStyle: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+                onPressed: () {
+                  setState(() {
+                    storyBrain.nextStory(1);
+                  });
+                },
+                child: Text(
+                  storyBrain.getChoice1(),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Expanded(
+              flex: 2,
+              child: Visibility(
+                visible: storyBrain.buttonVisibility(),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.blue[800],
+                    textStyle: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      storyBrain.nextStory(2);
+                    });
+                  },
+                  child: Text(
+                    storyBrain.getChoice2(),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/story.dart
@@ -0,0 +1,14 @@
+class Story {
+  late String storyTitle;
+  late String choice1;
+  late String choice2;
+
+  Story(
+      {required this.storyTitle,
+      required this.choice1,
+      required this.choice2}) {
+    storyTitle = storyTitle;
+    choice1 = choice1;
+    choice2 = choice2;
+  }
+}
